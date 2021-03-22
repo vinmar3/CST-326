fff4eea1e6e63c65480abad1df625a6eeec1664a
Vincent Martin <78059202+vinmar3@users.noreply.github.com>
2021-03-21 17:08:22 -0700

    Space Invader Part 2

---
+++ Space Invader Part 2/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Transform allEnemies;
+    public float speed = 0.5f;
+
+    void Start()
+    {
+        InvokeRepeating("MoveEnemy", 0.1f, 0.5f);
+        allEnemies = GetComponent<Transform>();
+    }
+
+    void MoveEnemy()
+    {
+        allEnemies.position += Vector3.right * speed;
+
+        foreach (Transform enemy in allEnemies)
+        {
+            if (enemy.position.x < -8 || enemy.position.x > 8)
+            {
+                speed = -speed;
+                allEnemies.position += Vector3.down * 0.25f;
+                return;
+            }
+            if(enemy.position.y <= -3)
+            {
+                GameObject.Find("Player").GetComponent<Player>().PlayerDead();
+            }
+        }
+
+        if (allEnemies.childCount == 0)
+        {
+            GameObject.Find("ToCredits").GetComponent<MainMenu>().Credits();
+        }
+    }
+}
